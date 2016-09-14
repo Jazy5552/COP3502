7865116597b510a262ccdbd85a96f497a571cb55
Jazy Llerena <jazy555@hotmail.com>
2016-09-14 17:06:26 -0400

    Start greatest common denominator exercise

---
+++ Lab1/gcd.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int getSmallest(int, int);
+int getLargest(int, int);
+
+int main() {
+	int in1, in2;
+	cout << "#";
+	return 0;
+}
+
+int getSmallest(int num1, int num2) {
+	
+}
+
