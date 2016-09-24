1145753d77440dae861f6bc8b6655126a4d8f19a
Jazy Llerena <jazy555@hotmail.com>
2016-09-24 03:27:11 -0400

    Modify algorithm to better show 2d pointer arrays in c++

--- Lab3/pointerArrayTest.cpp
+++ Lab3/pointerArrayTest.cpp
@@ -3,32 +3,35 @@
 using namespace std;
 
 int main() {
-	int width = 8;
-	int height = 6;
+	int width = 5;
+	int height = 3;
 
-	int** arr = new int*[width];
-	for (int i=0; i<width; i++) {
-		arr[i] = new int[height];
+	int** arr = new int*[height];
+	for (int i=0; i<height; i++) {
+		arr[i] = new int[width];
 	}
 
+	cout << "Created" << endl;
 	
 	for (int i=0; i<height; i++) {
 		for (int j=0; j<width; j++) {
-			arr[i][j] = 10*i + j;
+			arr[i][j] = width*i + j;
 		}
 	}
+
+	cout << "Created" << endl;
 
 	//Print addresses
 	for (int i=0; i<height; i++) {
 		for (int j=0; j<width; j++) {
-			cout << &arr[i][j] << "\t";
+			cout << arr[i][j] << "\t";
 		}
 		cout << endl;
 	}
 
 	//Print values by dereferencing the pointers
-	for (int i=0; i<height*width; i++) {
-		cout << *(*(arr)+i) << "\t";
+	for (int i=0; i<height; i++) {
+		cout << (*arr)[i] << "\t"; //DOESNT WORK!!! Every row has some addresses of gap making this fail!
 		if ((i+1) % width == 0)
 			cout << endl;
 	}
