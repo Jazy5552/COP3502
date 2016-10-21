9778bd36539b9f4344ec6fa9b9bbe22bfa367686
jazy5552 <jazy555@hotmail.com>
2016-10-21 13:03:22 -0400

    Create nodedata struct to be stored in the nodes

---
+++ Assignment2/nodedata.h
@@ -0,0 +1,23 @@
+#ifndef NODEDATA_H
+#define NODEDATA_H
+#include <string>
+
+// Custom node data structure that will be stored in the nodes
+typedef struct NodeData { 
+	std::string pName; // Name of the process using this node block
+	bool free; // Wether the node is being used or not at the moment
+	int blockSize; // Number of 4k blocks pertaining to this node.
+
+	//Constructor
+	NodeData(std::string name, bool f, int bs) : pName(name), free(f), blockSize(bs) {}
+
+	// Overload for comparing NodeDatas
+	// @Overload
+	bool operator==(const NodeData &b) const {
+		return (pName.compare(b.pName) == 0
+				&& free == b.free
+				&& blockSize == b.blockSize) ? true : false;
+	}
+}NodeData;
+
+#endif
